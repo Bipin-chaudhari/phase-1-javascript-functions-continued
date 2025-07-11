b7526744506fe611ed62e30a12d61545eb0d57e5
+ <+>
2025-07-11 23:34:57 +1000

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun) {
+    return `This Saturday, I want to ${fun ? fun : "roller-skate"}!`
+}
+
+function mondayWork(fun) {
+    return `This Monday, I will ${fun ? fun : "go to the office"}.`
+}
+
+function wrapAdjective(special) {
+
+    return function (fun) {
+        return `You are ${special + fun + special}!`
+    }
+}
